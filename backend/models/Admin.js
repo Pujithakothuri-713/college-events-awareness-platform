f0488e1ee703a4f05ce8eeacc695068e32f5e504
Pujitha <pujithakothuri713@gmail.com>
2026-06-11 21:31:56 +0530

    Add admin OTP support

--- backend/models/Admin.js
+++ backend/models/Admin.js
@@ -1,8 +1,31 @@
 const mongoose = require("mongoose");
 
-const adminSchema = new mongoose.Schema({
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-});
+const adminSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+
+    password: {
+      type: String,
+      required: true,
+    },
+
+    otp: {
+      type: String,
+      default: null,
+    },
+
+    otpExpiry: {
+      type: Date,
+      default: null,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
 
 module.exports = mongoose.model("Admin", adminSchema);
